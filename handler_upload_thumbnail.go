25220554b540822f7708d1c488ef8616f3014983
Ohne-Dich <lawerenz.f@gmail.com>
2025-06-15 13:28:19 +0200

    great, we store img as binary now - sucks, next!

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
@@ -62,17 +63,15 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	videoThumbnails[videoID] = thumbnail{
-		data:      img_data,
-		mediaType: mediaType,
-	}
-
-	url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
-	video.ThumbnailURL = &url
+	//storing the thumbnail in the SQLite as raw binary ... yay
+	// https://www.boot.dev/lessons/78d69134-fd38-44fd-b764-50348e81097f
+	Base64 := base64.StdEncoding.EncodeToString(img_data)
+	DataURL := fmt.Sprintf("data:%s;base64,%s", mediaType, Base64)
+	//url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	video.ThumbnailURL = &DataURL
 
 	err = cfg.db.UpdateVideo(video)
 	if err != nil {
-		delete(videoThumbnails, videoID)
 		respondWithError(w, http.StatusInternalServerError, "Could not update video", err)
 		return
 	}
